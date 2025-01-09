3080811ac4b40152a042a0afa086b0c3a1308271
Constantin Denis <denis.io.constantin@gmail.com>
2025-01-09 09:32:42 +0200

    Update PluginEditor.cpp, added mute button

--- Gain/Source/PluginEditor.cpp
+++ Gain/Source/PluginEditor.cpp
@@ -17,153 +17,64 @@
   ==============================================================================
 */
 
-//[Headers] You can add your own extra header files here...
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
 //[/Headers]
 
-#include "PluginEditor.h"
-
-
-//[MiscUserDefs] You can add your own user definitions and misc code here...
-//[/MiscUserDefs]
-
-//==============================================================================
-GainAudioProcessorEditor::GainAudioProcessorEditor (GainAudioProcessor* ownerFilter)
-    : AudioProcessorEditor(ownerFilter)
-{
-    //[Constructor_pre] You can add your own custom stuff here..
-    //[/Constructor_pre]
-
-    gainSlider.reset (new juce::Slider ("Gain Slider"));
-    addAndMakeVisible (gainSlider.get());
-    gainSlider->setRange (-70, 0, 0.1);
-    gainSlider->setSliderStyle (juce::Slider::LinearHorizontal);
-    gainSlider->setTextBoxStyle (juce::Slider::TextBoxLeft, false, 40, 20);
-    gainSlider->addListener (this);
-
-    gainSlider->setBounds (104, 96, 271, 32);
-
-    gainLabel.reset (new juce::Label ("Gain Label",
-                                      TRANS ("Nivel [dB]")));
-    addAndMakeVisible (gainLabel.get());
-    gainLabel->setFont (juce::Font (15.00f, juce::Font::plain).withTypefaceStyle ("Regular"));
-    gainLabel->setJustificationType (juce::Justification::centredLeft);
-    gainLabel->setEditable (false, false, false);
-    gainLabel->setColour (juce::TextEditor::textColourId, juce::Colours::black);
-    gainLabel->setColour (juce::TextEditor::backgroundColourId, juce::Colour (0x00000000));
-
-    gainLabel->setBounds (8, 96, 86, 32);
-
-
-    //[UserPreSize]
-    //[/UserPreSize]
-
-    setSize (400, 200);
-
-    
-    //[Constructor] You can add your own custom stuff here..
-    getProcessor()->RequestUIUpdate();
-    startTimer(200); //porneste temporizatorul cu un interval de 200 ms
-    //[/Constructor]
-}
-
-GainAudioProcessorEditor::~GainAudioProcessorEditor()
-{
-    //[Destructor_pre]. You can add your own custom destruction code here..
-    //[/Destructor_pre]
-
-    gainSlider = nullptr;
-    gainLabel = nullptr;
-
-
-    //[Destructor]. You can add your own custom destruction code here..
-    //[/Destructor]
-}
-
-//==============================================================================
-void GainAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    //[UserPrePaint] Add your own custom painting code here..
-    //[/UserPrePaint]
-
-    g.fillAll (juce::Colour (0xff35001c));
-
-    //[UserPaint] Add your own custom painting code here..
-    //[/UserPaint]
-}
-
-void GainAudioProcessorEditor::resized()
-{
-    //[UserPreResize] Add your own custom resize code here..
-    //[/UserPreResize]
-
-    //[UserResized] Add your own custom resize handling here..
-    //[/UserResized]
-}
-
-void GainAudioProcessorEditor::sliderValueChanged (juce::Slider* sliderThatWasMoved)
-{
-    //[UsersliderValueChanged_Pre]
-     GainAudioProcessor* ourProcessor = getProcessor();
-    //[/UsersliderValueChanged_Pre]
-
-    if (sliderThatWasMoved == gainSlider.get())
-    {
-        //[UserSliderCode_gainSlider] -- add your slider handling code here..
-        float normValue = powf(10.f, (float)gainSlider->getValue() / 20.f);
-        ourProcessor->setParameter(GainAudioProcessor::gain, normValue);
-        //[/UserSliderCode_gainSlider]
-    }
-
-    //[UsersliderValueChanged_Post]
-    //[/UsersliderValueChanged_Post]
-}
-
-
-
-//[MiscUserCode] You can add your own definitions of your custom methods or any other code here...
-    void GainAudioProcessorEditor::timerCallback(){
-        GainAudioProcessor* ourProcessor = getProcessor(); //se schimba datele dorite intre elementele interfetei grafice si modulul “ourProcessor”
-        if (ourProcessor->NeedsUIUpdate())
-        {
-            float dbValue = 20.f * log10(ourProcessor->getParameter(GainAudioProcessor::gain));
-            gainSlider->setValue(dbValue, juce::dontSendNotification);
-            ourProcessor->ClearUIUpdateFlag();
-        }
-    }
-//[/MiscUserCode]
 
 
 //==============================================================================
-#if 0
-/*  -- Projucer information section --
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
 
-    This is where the Projucer stores the metadata that describe this GUI layout, so
-    make changes in here at your peril!
-
-BEGIN_JUCER_METADATA
-
-<JUCER_COMPONENT documentType="Component" className="GainAudioProcessorEditor"
-                 componentName="" parentClasses="public AudioProcessorEditor, public Timer"
-                 constructorParams="GainAudioProcessor* ownerFilter" variableInitialisers="AudioProcessorEditor(ownerFilter)"
-                 snapPixels="8" snapActive="1" snapShown="1" overlayOpacity="0.330"
-                 fixedSize="1" initialWidth="400" initialHeight="200">
-  <BACKGROUND backgroundColour="ff35001c"/>
-  <SLIDER name="Gain Slider" id="8f77fd08b320037f" memberName="gainSlider"
-          virtualName="" explicitFocusOrder="0" pos="104 96 271 32" min="-70.0"
-          max="0.0" int="0.1" style="LinearHorizontal" textBoxPos="TextBoxLeft"
-          textBoxEditable="1" textBoxWidth="40" textBoxHeight="20" skewFactor="1.0"
-          needsCallback="1"/>
-  <LABEL name="Gain Label" id="4892b907a93fecb5" memberName="gainLabel"
-         virtualName="" explicitFocusOrder="0" pos="8 96 86 32" edTextCol="ff000000"
-         edBkgCol="0" labelText="Nivel [dB]" editableSingleClick="0" editableDoubleClick="0"
-         focusDiscardsChanges="0" fontname="Default font" fontsize="15.0"
-         kerning="0.0" bold="0" italic="0" justification="33"/>
-</JUCER_COMPONENT>
-
-END_JUCER_METADATA
+    Describe your class and how it works here!
+                                                                    //[/Comments]
 */
-#endif
+class GainAudioProcessorEditor  : public AudioProcessorEditor,
+                                  public Timer,
+                                  public juce::Slider::Listener,
+                                  public juce::Button::Listener
+{
+public:
+    //==============================================================================
+    GainAudioProcessorEditor (GainAudioProcessor* ownerFilter);
+    ~GainAudioProcessorEditor() override;
 
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    void timerCallback();
+    GainAudioProcessor* getProcessor() const{
+        return static_cast <GainAudioProcessor*>(getAudioProcessor());
+    }
+    //[/UserMethods]
+
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+    void sliderValueChanged (juce::Slider* sliderThatWasMoved) override;
+    void buttonClicked (juce::Button* buttonThatWasClicked) override;
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
+
+    //==============================================================================
+    std::unique_ptr<juce::Slider> gainSlider;
+    std::unique_ptr<juce::Label> gainLabel;
+    std::unique_ptr<juce::Slider> delaySlider;
+    std::unique_ptr<juce::Label> delayLabel;
+    std::unique_ptr<juce::ToggleButton> muteButton;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GainAudioProcessorEditor)
+};
 
 //[EndFile] You can add extra defines here...
 //[/EndFile]
