49268ec1aeb5f4a28da65bbabacb33aebb177d8c
Constantin Denis <denis.io.constantin@gmail.com>
2025-02-05 13:54:45 +0200

    Update PluginProcessor.h

--- Gain/Source/PluginProcessor.h
+++ Gain/Source/PluginProcessor.h
@@ -58,22 +58,46 @@ public:
     void setParameter(int index, float newValue) override;
     const String getParameterName(int index) override;
     const String getParameterText(int index) override;
-    enum Parameters { gain = 0, delay = 1, frecv = 2, totalNumParam };
+    enum Parameters { gain = 0, delay = 1, frecv = 2, compRatio = 3, threshold = 4, totalNumParam };
     bool NeedsUIUpdate() { return UIUpdateFlag; };
     void RequestUIUpdate() { UIUpdateFlag = true; };
     void ClearUIUpdateFlag() { UIUpdateFlag = false; };
     bool isMuted;
-    std::vector<float> dl;
+    bool isDelay;
+    
 private:
     //==============================================================================
+    // pentru filtru
+    float a[3] = {}, b[3] = {};
+    float w[2][2];
 
+    //pentru delay
     int M_max;
-    
+    std::vector<float> dl;
     int index_IN;
     int index_OUT;
     int Fs;
     int M;
 
+    // pentru compressor
+    float rmsAverage;
+    float attackTime;
+    float releaseTime;
+    float delayTime;
+    int delaySamples;
+    float rmsLevel;
+    float smoothGain;
+    int bufferIndex;
+    std::vector<float> buffer;
+    float alphaAvg;
+    float alphaAt;
+    float alphaRt;
+
+    void GainAudioProcessor::resetDelay();
+    float GainAudioProcessor::filterSampleFTJ(float in, int ch);
+    void GainAudioProcessor::coeffCalc(float ft, float fs);
+    void GainAudioProcessor::resetCompression();
+    void GainAudioProcessor::alphaCalc(float fs);
     float UserParams[totalNumParam];
     bool UIUpdateFlag; //Valoare booleana pentru reimprospatarea interfetei grafice
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainAudioProcessor)
